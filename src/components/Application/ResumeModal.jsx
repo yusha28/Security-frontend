f6826676c91b7541a0a221e653192182f3926f9f
yusha28 <105900973+yusha28@users.noreply.github.com>
2025-02-03 01:17:53 +0545

    Resume

--- src/components/Application/ResumeModal.jsx
+++ src/components/Application/ResumeModal.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+// import React from "react";
 
-const ResumeModal = ({ imageUrl, onClose }) => {
-  return (
-    <div className="resume-modal">
-      <div className="modal-content">
-        <span className="close" onClick={onClose}>
-          &times;
-        </span>
-        <img src={imageUrl} alt="resume" />
-      </div>
-    </div>
-  );
-};
+// const ResumeModal = ({ imageUrl, onClose }) => {
+//   return (
+//     <div className="resume-modal">
+//       <div className="modal-content">
+//         <span className="close" onClick={onClose}>
+//           &times;
+//         </span>
+//         <img src={imageUrl} alt="resume" />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ResumeModal;
+// export default ResumeModal;
