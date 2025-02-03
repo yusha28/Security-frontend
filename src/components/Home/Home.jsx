d39fafaaf66016f8130b2571c5d0bbee12a63a1d
yusha28 <105900973+yusha28@users.noreply.github.com>
2025-02-03 14:15:12 +0545

    Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
-import HeroSection from "./HeroSection";
-import HowItWorks from "./HowItWorks";
-import PopularCategories from "./PopularCategories";
-import PopularCompanies from "./PopularCompanies";
-import { Context } from "../context/UserContextProvider";
+// import React from "react";
+// import { useContext } from "react";
+// import { Navigate } from "react-router-dom";
+// import HeroSection from "./HeroSection";
+// import HowItWorks from "./HowItWorks";
+// import PopularCategories from "./PopularCategories";
+// import PopularCompanies from "./PopularCompanies";
+// import { Context } from "../context/UserContextProvider";
 
-const Home = () => {
-  const { isAuthorized } = useContext(Context);
-  if (!isAuthorized) {
-    return <Navigate to={"/login"} />;
-  }
-  return (
-    <>
-      <section className="homePage page">
-        <HeroSection />
-        <HowItWorks />
-        <PopularCategories />
-        <PopularCompanies />
-      </section>
-    </>
-  );
-};
+// const Home = () => {
+//   const { isAuthorized } = useContext(Context);
+//   if (!isAuthorized) {
+//     return <Navigate to={"/login"} />;
+//   }
+//   return (
+//     <>
+//       <section className="homePage page">
+//         <HeroSection />
+//         <HowItWorks />
+//         <PopularCategories />
+//         <PopularCompanies />
+//       </section>
+//     </>
+//   );
+// };
 
-export default Home;
+// export default Home;
